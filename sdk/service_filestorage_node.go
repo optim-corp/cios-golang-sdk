d1a3b56a2783cefbf98ea99b5ff2ce1014ddf980
kazuhiro.seida <kazuhiro.seida@optim.co.jp>
2021-01-28 12:51:09 +0900

    wip node test

--- sdk/service_filestorage_node.go
+++ sdk/service_filestorage_node.go
@@ -41,19 +41,14 @@ func (self FileStorage) GetNodesAll(bucketID string, params cios.ApiGetNodesRequ
 		err         error
 		offset      = int64(0)
 		_limit      = int64(1000)
-		getFunction = func(offset *int64) (cios.MultipleNode, *_nethttp.Response, error) {
-			if offset != nil {
-				params.P_offset = offset
-			}
-			tlimit := xmath.MinInt64(_limit, 1000)
-			params.P_limit = &tlimit
-			return self.GetNodes(bucketID, params, ctx)
+		getFunction = func(offset int64) (cios.MultipleNode, *_nethttp.Response, error) {
+			return self.GetNodes(bucketID, params.Limit(xmath.MinInt64(_limit, 1000)).Offset(offset), ctx)
 		}
 	)
 	if params.P_limit != nil {
 		_limit = *params.P_limit
 		for {
-			res, httpRes, err := getFunction(&offset)
+			res, httpRes, err := getFunction(offset)
 			if err != nil {
 				return nil, httpRes, err
 			}
@@ -65,13 +60,13 @@ func (self FileStorage) GetNodesAll(bucketID string, params cios.ApiGetNodesRequ
 			}
 		}
 	} else {
-		res, httpRes, err := getFunction(&offset)
+		res, httpRes, err := getFunction(offset)
 		if err != nil {
 			return nil, httpRes, err
 		}
 		result = append(result, res.Nodes...)
 		for offset = int64(1000); offset < res.Total; offset += 1000 {
-			res, httpRes, err = getFunction(&offset)
+			res, httpRes, err = getFunction(offset)
 			if err != nil {
 				return nil, httpRes, err
 			}
