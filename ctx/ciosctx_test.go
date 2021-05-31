cebc49763b74bb8ee8c0770ac9fe1fbd78e103e4
kazuhiro.seida <kazuhiro.seida@optim.co.jp>
2021-05-31 13:18:54 +0900

    add ctx test

--- ctx/ciosctx_test.go
+++ ctx/ciosctx_test.go
@@ -1,20 +1,28 @@
-package ciosctx
+package ciosctx_test
 
 import (
+	"context"
 	"reflect"
 	"testing"
+
+	cnv "github.com/fcfcqloow/go-advance/convert"
+
+	ciosctx "github.com/optim-corp/cios-golang-sdk/ctx"
 )
 
 func TestBackground(t *testing.T) {
 	tests := []struct {
 		name string
-		want RequestCtx
+		want ciosctx.RequestCtx
 	}{
-		// TODO: Add test cases.
+		{
+			name: "Test Background",
+			want: context.Background(),
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := Background(); !reflect.DeepEqual(got, tt.want) {
+			if got := ciosctx.Background(); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("Background() = %v, want %v", got, tt.want)
 			}
 		})
@@ -23,18 +31,33 @@ func TestBackground(t *testing.T) {
 
 func TestToken(t *testing.T) {
 	type args struct {
-		ctx RequestCtx
+		ctx ciosctx.RequestCtx
 	}
 	tests := []struct {
 		name      string
 		args      args
 		wantToken *string
 	}{
-		// TODO: Add test cases.
+		{
+			name: "Test CTX Token",
+			args: args{
+				ctx: context.Background(),
+			},
+			wantToken: nil,
+		},
+		{
+			name: "Test CTX Token",
+			args: args{
+				ctx: func() ciosctx.RequestCtx {
+					return ciosctx.WithToken(ciosctx.Background(), "testtest")
+				}(),
+			},
+			wantToken: cnv.StrPtr("testtest"),
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if gotToken := Token(tt.args.ctx); !reflect.DeepEqual(gotToken, tt.wantToken) {
+			if gotToken := ciosctx.Token(tt.args.ctx); !reflect.DeepEqual(gotToken, tt.wantToken) {
 				t.Errorf("Token() = %v, want %v", gotToken, tt.wantToken)
 			}
 		})
@@ -43,19 +66,29 @@ func TestToken(t *testing.T) {
 
 func TestWithToken(t *testing.T) {
 	type args struct {
-		ctx   RequestCtx
+		ctx   ciosctx.RequestCtx
 		token string
 	}
 	tests := []struct {
 		name string
 		args args
-		want RequestCtx
+		want ciosctx.RequestCtx
 	}{
-		// TODO: Add test cases.
+		{
+			name: "Test With Token",
+			args: args{
+				ctx:   ciosctx.Background(),
+				token: "Test Token",
+			},
+			want: func() ciosctx.RequestCtx {
+				return ciosctx.WithToken(context.Background(), "Test Token")
+				// TODO: 考える
+			}(),
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := WithToken(tt.args.ctx, tt.args.token); !reflect.DeepEqual(got, tt.want) {
+			if got := ciosctx.WithToken(tt.args.ctx, tt.args.token); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("WithToken() = %v, want %v", got, tt.want)
 			}
 		})
